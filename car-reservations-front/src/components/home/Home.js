0e77abc568b8665b2399d8d3f25ff3599d5cb86b
polon59 <przemyslaw.polczak@gmail.com>
2019-06-11 17:05:11 +0200

    Add components folders for each page

---
+++ car-reservations-front/src/components/home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component{
+
+    constructor(props){
+        super();
+    }
+
+    render(){
+        <div>
+            <h3>Homepage</h3>
+        </div>
+    }
+}
