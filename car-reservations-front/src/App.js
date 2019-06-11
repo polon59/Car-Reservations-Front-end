90b2a923a51bdbcecdcacf311e7ec68ddc2a4e3f
polon59 <przemyslaw.polczak@gmail.com>
2019-06-11 17:50:15 +0200

    Add rendering navigation bar

--- car-reservations-front/src/App.js
+++ car-reservations-front/src/App.js
@@ -1,5 +1,6 @@
 import {BrowserRouter as Router,Route} from 'react-router-dom';
 import React from 'react';
+import Navbar from './components/common/navbar/Navbar'
 import Home from './components/home/Home';
 import Offer from './components/offer/Offer';
 import Contact from './components/contact/Contact';
@@ -9,7 +10,7 @@ function App() {
   return (
     <Router>
         <div className="App">
-        <h1>NAVBAR</h1>
+          <Navbar/>
           <Route 
             exact path="/" 
             render={(props) => 
