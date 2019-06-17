e18ccacfa553f920558ce828ed4b8dd8e65092cf
polon59 <przemyslaw.polczak@gmail.com>
2019-06-17 15:58:49 +0200

    Add DBAccess import

--- car-reservations-front/src/App.js
+++ car-reservations-front/src/App.js
@@ -4,32 +4,42 @@ import Navbar from './components/common/navbar/Navbar'
 import Home from './components/home/Home';
 import Offer from './components/offer/Offer';
 import Contact from './components/contact/Contact';
+import DBAccess from './Fetch/DBAccess';
 import './App.css';
 
-function App() {
-  return (
-    <Router>
-        <div className="App">
-          <Navbar/>
-          <Route 
-            exact path="/" 
-            render={(props) => 
-              <Home/>
-              }
-          />
-          <Route 
-            path="/offer"
-            render={(props) => 
-              <Offer/>}
-          />
-          <Route 
-            path="/contact" 
-            render={(props) => 
-              <Contact/>}
-          />
-        </div>
-      </Router>
-  );
+class App extends React.Component{
+
+  constructor(props){
+    super();
+    this.DBAccess = new DBAccess();
+  }
+
+  render(){
+    return (
+      <Router>
+          <div className="App">
+            <Navbar/>
+            <Route 
+              exact path="/" 
+              render={(props) => 
+                <Home/>
+                }
+            />
+            <Route 
+              path="/offer"
+              render={(props) => 
+                <Offer DBAccess = {this.DBAccess}/>}
+            />
+            <Route 
+              path="/contact" 
+              render={(props) => 
+                <Contact/>}
+            />
+          </div>
+        </Router>
+    );
+  }
+  
 }
 
 export default App;
