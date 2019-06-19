cef29d06de571d983760ecdd8de379a829bd9d8b
polon59 <przemyslaw.polczak@gmail.com>
2019-06-19 18:43:16 +0200

    Add link to car details page for each cars table cell

--- car-reservations-front/src/components/offer/CarsTableCell.js
+++ car-reservations-front/src/components/offer/CarsTableCell.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import {Link} from 'react-router-dom';
+import ViewCar from './ViewCar';
 
 const CarsTableCell = (props) =>{
-    const {brand,model,category,price,imageSource} = props.carData;
+    const {carId,brand,model,category,price,imageSource} = props.carData;
 
 
     return(
         <Grid item xs={12} sm={6} md={4} className="carsTableCell">
-            <Paper className="carsTablePaper">
-                <div className="imageWrapper">
-                    <img src={imageSource} alt={model}/>
-                </div>
-                <h3>{`${brand} ${model}`}</h3>
-                <p>{category}</p>
-                <h4>{`${price} 	€/Day`}</h4>
-            </Paper>
+            <Link to={`/offer/carViewCar/${carId}`} >
+                <Paper className="carsTablePaper">
+                    <div className="imageWrapper">
+                        <img src={imageSource} alt={model}/>
+                    </div>
+                    <h3>{`${brand} ${model}`}</h3>
+                    <p>{category}</p>
+                    <h4>{`${price} 	€/Day`}</h4>
+                </Paper>
+            </Link>
         </Grid>
     )
 }
