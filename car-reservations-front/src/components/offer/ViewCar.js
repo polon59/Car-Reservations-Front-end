561de4d9d481ed2fa0c9a4d79607acad41148af9
polon59 <przemyslaw.polczak@gmail.com>
2019-06-19 18:43:57 +0200

    Change 'ViewCar' component from function to class

--- car-reservations-front/src/components/offer/ViewCar.js
+++ car-reservations-front/src/components/offer/ViewCar.js
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const ViewCar = (props) =>{
-    console.log(props.match.params.id)
-    // const {carData} = props;
-    // console.log(props.route.carData)
-    return(
-        <div>
-            <h4>VievCar</h4>
-        </div>
-    )
+class ViewCar extends Component{
+
+    constructor(props){
+        super();
+        this.DBAccess = props.DBAccess;
+        this.state = {
+            selectedCarID : props.id,
+            selectedCarDetails : null
+        }
+    }
+
+    componentWillMount() {
+        const {selectedCarID} = this.state;
+        this.DBAccess.getCarDetailsById(selectedCarID)
+        .then((result)=>{
+            this.setState({selectedCarDetails:result});
+        })
+    }
+    
+    render(){
+        const {selectedCarDetails} = this.state;
+
+        if (!selectedCarDetails){
+            return(
+                <div>
+                    <h4>Loading...</h4>
+                </div>
+            )
+        }
+        const{carId,brand,model,category,price,imageSource,year,seats,doors,automaticGearbox,automaticAC} = selectedCarDetails;
+        return(
+            <div>
+                <div>
+                    <img src={imageSource} alt={model}/>
+                </div>
+                <h3>{`${brand} ${model}`}</h3>
+                <p>{category}</p>
+                <h4>{`year: ${year}`}</h4>
+                <h4>{`seats: ${seats}`}</h4>
+                <h4>{`doors: ${doors}`}</h4>
+                <h4>{`automaticGearbox: ${automaticGearbox}`}</h4>
+                <h4>{`automaticAC: ${automaticAC}`}</h4>
+            </div>
+        )
+    }
 }
 
 export default ViewCar;
