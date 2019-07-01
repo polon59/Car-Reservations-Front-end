f2426fa0bdf7d13aab7ee35075ea475556a31d4b
polon59 <przemyslaw.polczak@gmail.com>
2019-07-01 23:44:30 +0200

    Add new footer content

--- car-reservations-front/src/components/common/footer/Footer.js
+++ car-reservations-front/src/components/common/footer/Footer.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
 
 const Footer = (props) =>{
     return(
         <footer>
-            <ul>
-                <li>Some info</li>
-                <li>Some info</li>
-                <li>Some info</li>
-            </ul>
+            <Grid container spacing={0} justify="center">
+                <Grid item xs={12} md={4}>
+                    <p>Contact</p> 
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    Designed and built by Seweryn91 & <a href="http://"></a> polon59
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <p>About</p>
+                </Grid>
+            </Grid>
         </footer>
     )
 }
