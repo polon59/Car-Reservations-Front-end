5189c1eb5432271ebbbb285c03c1dbaa485bf29c
polon59 <przemyslaw.polczak@gmail.com>
2019-07-01 22:06:20 +0200

    Add 'footer' component

---
+++ car-reservations-front/src/components/common/footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = (props) =>{
+    return(
+        <footer>
+            <ul>
+                <li>Some info</li>
+                <li>Some info</li>
+                <li>Some info</li>
+            </ul>
+        </footer>
+    )
+}
+
+export default Footer;
